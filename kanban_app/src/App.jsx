5ad3cf564b72e087f8602a30c1ddd6b378e45d2c
Ashwin <ashwinsriram11@gmail.com>
2023-12-22 10:13:34 +0530

    Fetched data successfully

--- kanban_app/src/App.jsx
+++ kanban_app/src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react"
 
 function App() {
+  const [data,setData] = useState({});
+  
+  useEffect(()=>{
+    const fetchData = async () =>{
+      try{
+        const response = await fetch(import.meta.env.VITE_REACT_APP_API_URL);
+        const result = await response.json();
+        setData(result);
+      }catch(error){
+        console.log("Error fetching data : ",error);
+      }
+    };
+    fetchData();
+  },[]);
+
+  const {tickets,users} = data;
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
     </>
   )
 }
